6795680722b31f5e8b9144c12a1cec7aaa87adec
Michał Foks <michalfoks3@gmail.com>
2022-10-19 23:23:27 +0200

    Script for running contract locally (draft of SC)

--- contracts/tickeD.sol
+++ contracts/tickeD.sol
@@ -1,24 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "hardhat/console.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract tickeD is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MyNFT", "NFT") {}
+    constructor() ERC721("tickeD", "tD") {
+        console.log("runs");
+    }
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
+    // function createNFT(address recipient, string memory tokenURI)
+    // function that enables users to create NFT
+    function createNFT()
+        public
     {
+        uint256 newItemId = _tokenIds.current();
+        // mint NFT to user
+        _safeMint(msg.sender, newItemId);
         _tokenIds.increment();
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
+        //_setTokenURI(newItemId, tokenURI);
     }
+
+    // Set the NFT's metadata
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    require(_exists(_tokenId));
+    return "blah";
+  }
+
+
 }
