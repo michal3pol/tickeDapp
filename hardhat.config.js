e4a50c1ea65517d565d74602fe6dd851a9e4571d
Michał Foks <michalfoks3@gmail.com>
2022-11-02 23:39:20 +0100

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,28 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
   networks: {
     goerli: {
       url: process.env.QUICKNODE_API_KEY_URL,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
     },
+    hardhat: {
+      blockGasLimit: 10000000000,
+    }
   },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1
+      }
+    }
+  },
+  // settings: {
+  //   optimizer: {
+  //     enabled: true,
+  //     runs: 1
+  //   }
+  // }
 };
