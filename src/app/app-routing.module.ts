02982c9700fb4f2d8cbe1277998eece24c9bb81d
Michał Foks <michalfoks3@gmail.com>
2022-11-11 15:20:16 +0100

    Updates in forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateConcertComponent } from './components/concert-management/create-concert/create-concert.component';
 import { MyConcertsComponent } from './components/concert-management/my-concerts/my-concerts.component';
+import { WhitelistComponent } from './components/concert-management/whitelist/whitelist.component';
 import { MarketplaceComponent } from './components/marketplace/marketplace.component';
 import { ResellComponent } from './components/resell/resell.component';
 import { SellComponent } from './components/sell/sell.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'marketplace', component: MarketplaceComponent },
   { path: 'create-concert', canActivate:[AuthOrganizatorGuard], component: CreateConcertComponent },
   { path: 'my-concerts', canActivate:[AuthOrganizatorGuard], component: MyConcertsComponent },
+  { path: 'whitelist', component: WhitelistComponent },
 ];
 
 @NgModule({
