d4af414fe797c0cf53e2465a7392227e70e524c9
Michał Foks <michalfoks3@gmail.com>
2022-12-03 23:32:27 +0100

    Update hardhat config to ts

---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import { default as dotenv } from 'dotenv';
+
+dotenv.config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: process.env['QUICKNODE_API_KEY_URL'],
+      accounts: [process.env['WALLET_PRIVATE_KEY']!],
+    },
+    matic: {
+      url: process.env['ALCHEMY_MUMBAI_API_KEY'],
+      accounts: [process.env['WALLET_PRIVATE_KEY']!],
+    },
+    hardhat: {
+      blockGasLimit: 10000000000,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  // settings: {
+  //   optimizer: {
+  //     enabled: true,
+  //     runs: 1
+  //   }
+  // }
+};
+
+export default config;
