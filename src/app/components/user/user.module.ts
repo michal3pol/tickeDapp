347ee1b5949677671b333ee17ebf61594d143b51
Michał Foks <michalfoks3@gmail.com>
2022-12-28 13:14:54 +0100

    Update look & feel

--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MyOffersComponent } from './my-offers/my-offers.component';
 import { PipesModule } from 'src/app/pipes/pipes.module';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -30,6 +31,7 @@ import { PipesModule } from 'src/app/pipes/pipes.module';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
     FormsModule,
     PipesModule
   ]
