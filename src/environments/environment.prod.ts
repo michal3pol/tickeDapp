e81ec2ecb17f80ca48e7cc3bac8a8e618fc0d6e4
Michał Foks <michalfoks3@gmail.com>
2023-03-06 15:04:50 +0100

    Add github-pages & deploy app (#18)
    * Add angular-cli-ghpages
    
    * Add env variables and adjust prod build config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  contractTickeDFactoryAddress: "0x147E68755Bf177FD60451A4cDeDB15782b0d823B",
+  contractNftMarketplaceAddress: "0x2DF7a6C36e8eDeaAF59d09cb4230B660E0283E88",
+  alchemyApi: "https://eth-goerli.g.alchemy.com/v2/9XMQ5x8fQjeWNnufjSXkhh5TMLnCnStG",
 };
