1ca192164d62a83a881f0a8449f56164a7b030a6
Michał Foks <michalfoks3@gmail.com>
2023-11-28 23:47:18 +0100

    Add sepolia config and contracts addr

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  contractTickeDFactoryAddress: "0x147E68755Bf177FD60451A4cDeDB15782b0d823B",
-  contractNftMarketplaceAddress: "0x2DF7a6C36e8eDeaAF59d09cb4230B660E0283E88",
-  alchemyApi: "https://eth-goerli.g.alchemy.com/v2/9XMQ5x8fQjeWNnufjSXkhh5TMLnCnStG",
+  contractTickeDFactoryAddress: "0x28342CD0d663379D5d66370561aB3e0B6e6C4Fdd",
+  contractNftMarketplaceAddress: "0x3625E8bAD6F8C0a26330d9E39C670f4D6649386e",
+  alchemyApi: "https://eth-sepolia.g.alchemy.com/v2/rmg8QMbnUjFuHvPgiYHSsl653yN5FUMQ",
 };
 
 /*
