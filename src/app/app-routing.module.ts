923c2f9452a8c05b601b310f40c96c1facaaa6d0
Michał Foks <michalfoks3@gmail.com>
2024-04-12 17:11:35 +0200

    Use hashlocationstrategy for ipfs & update budgets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [RouterModule.forRoot(routes, {useHash: true}), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
